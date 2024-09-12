578cc5c098ece09515d6ed30b250475fc8087686
Sam <dayumsam22@gmail.com>
2024-09-12 03:43:50 +0000

    test

--- apps/wingchun/app.js
+++ apps/wingchun/app.js
@@ -1,4 +1,7 @@
-Bangle.setHRMPower(1);
-Bangle.on("HRM", function (hrm) {
-  Bluetooth.println(hrm);
-});
+// Bangle.setHRMPower(1);
+// Bangle.on("HRM", function (hrm) {
+//   Bluetooth.println(hrm);
+// });
+
+
+Bluetooth.println("hehe");
